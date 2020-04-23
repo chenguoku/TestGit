331e579e00cd5370b4986e85f92ca8c51df4471b
chenguoku <cgkhh1996@163.com>
2020-04-23 15:49:05 +0800

    test2 commit

---
+++ test_maven_spring/src/main/java/com/example/demo/controller/test2.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+/**
+ * @author chenguoku
+ * @version 1.0.0
+ * @ClassName test2.java
+ * @Description TODO
+ * @createTime 2020年04月23日 15:48:00
+ */
+public class test2 {
+}
